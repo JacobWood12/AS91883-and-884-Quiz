5db9f3f077d84fe7836a9edd3714eb1c935bd037
JacobWood12 <15755651-JacobWood12@users.noreply.replit.com>
2023-04-04 09:43:05 +0000

    Added test to ensure both player names are not the same.

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ print("This is a multi-player quiz, so make sure you're both here.")
 # Get player names.
 print(seperator)
 player_1 = str(input("Player 1 - what is your name? \n"))
-# Tests if player names have numbers in them.
+# Tests if player names have numbers in them, and rejects them if they are.
 if any(char.isdigit() for char in player_1):
   print("Please input a name with letters only.")
 else:
   player_2 = str(input("Player 2 - what about you? \n"))
   if any(char.isdigit() for char in player_2):
-    print("Please input a name with letters only.")
+    print("Please input a name with letters or characters only.")
+  # After testing if player names have numbers, tests if they're both identical and rejects them if they are.
   else:
-    print(f"Welcome, {player_1} and {player_2}, let's get started.")
+    if player_1 == player_2:
+      print("Please choose different names from each other.")
+    else:
+      print(f"Welcome, {player_1} and {player_2}, let's get started.")
 
 print(seperator)
