d6d99492dd680832630ed34e4388322def65f4df
JacobWood12 <15755651-JacobWood12@users.noreply.replit.com>
2023-04-04 07:44:46 +0000

    Added welcome message and ask users for names.

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-# Imports package to choose random answers
-import random
+# String of dashes to print to seperate text.
+seperator = str("--------------------")
+
+# Welcome messages.
+print("Welcome to this quiz!")
+print("Today, your topics will be geography, history, and politics.")
+print("This is a multi-player quiz, so make sure you're both here.")
+
+# Get player names.
+print(seperator)
+player_1 = str(input("Player 1 - what is your name? \n"))
+player_2 = str(input("Player 2 - what about you? \n"))
+print(f"Welcome, {player_1} and {player_2}, let's get started.")
+
+print(seperator)
