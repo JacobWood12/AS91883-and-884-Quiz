3e612efbb84ac83f323160a7cb06fb712730ed03
JacobWood12 <15755651-JacobWood12@users.noreply.replit.com>
2023-04-06 00:10:19 +0000

    Added question database and updated README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AS91883-84-Quiz
 
-My assesment for Year 11 Computer Science, covering Assesment Standards 91883 and 91884.
+### My assesment for Year 11 Computer Science, covering Assesment Standards 91883 and 91884.
 I will be creating a multi-player, multi choice quiz.
 The topics are geography, history and politics.
 This is due at the end of Term 1, on the 6th of March 2023.
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # String of dashes to print to seperate text.
-seperator = str("--------------------")
+separator = str("--------------------")
 
 # Welcome messages.
 print("Welcome to this quiz!")
@@ -7,9 +7,9 @@ print("Today, your topics will be geography, history, and politics.")
 print("This is a multi-player quiz, so make sure you're both here.")
 
 # Get player names.
-print(seperator)
+print(separator)
 player_1 = str(input("Player 1 - what is your name? \n"))
-#2 Tests if player names have numbers in them, and rejects them if they are.
+# Tests if player names have numbers in them, and rejects them if they are.
 if any(char.isdigit() for char in player_1):
   print("Please input a name with letters only.")
 else:
@@ -23,7 +23,34 @@ else:
     else:
       print(f"Welcome, {player_1} and {player_2}, let's get started.")
 
-print(seperator)
+print(separator)
 
 # Question database.
-# TODO
+questions = [
+  # Geography section.
+  "How many states are in the USA?",
+  "What continent is Nepal in?",
+  "What is the capital of Scotland?",
+  "Where is Dunedin?",
+  "What is the line drawn around the Earth halfway between both poles called?",
+  "What is the population of China?",
+  "Which of these oceans is the smallest?",
+  # History section.
+  "When did World War II start?",
+  "How long ago was the extinction of the dinosaurs?",
+  "When was the Treaty of Waitangi signed?",
+  "Who was the American Civil War between?",
+  "What language did they speak in Ancient Rome?",
+  "How many capital cities has New Zealand had?",
+  "When was Google founded?",
+  # Politics section.
+  "As of April 2023, who is the President of the United States?",
+  "How often are elections held in New Zealand?",
+  "About how many countries are there in the world?",
+  "Where was the first democracy?",
+  "What is the voting age in New Zealand?",
+  "What is a dictatorship?",
+  "Random bonus point!",
+]
+
+# Question chooser.
